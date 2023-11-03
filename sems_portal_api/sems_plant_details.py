b36b451478db30d89a78d4084878611dfd21e3a3
Andrew Gierens <apgierens@gmail.com>
2023-11-03 10:00:06 +0000

    Cleaned up structure and added missing library

--- sems_portal_api/sems_plant_details.py
+++ sems_portal_api/sems_plant_details.py
@@ -5,7 +5,7 @@ from typing import Any
 from aiohttp import ClientSession
 
 
-async def get_plantDetails(
+async def get_plant_details(
     session: ClientSession, power_station_id: str, token: str
 ) -> Any:
     """Get powerplant details."""
@@ -25,6 +25,21 @@ async def get_powerflow(
     """Get the powerflow data."""
 
     url = "https://au.semsportal.com/api/v2/PowerStation/GetPowerflow"
+    headers = {"Content-Type": "application/json", "Token": token}
+    body = {"PowerStationId": power_station_id}
+
+    response = await session.post(url, headers=headers, json=body)
+    response_data = await response.json()
+
+    return response_data["data"]
+
+
+async def get_inverter_details(
+    session: ClientSession, power_station_id: str, token: str
+) -> Any:
+    """Get the inverter data."""
+
+    url = "https://au.semsportal.com/api/v3/PowerStation/GetInverterAllPoint"
     headers = {"Content-Type": "application/json", "Token": token}
     body = {"PowerStationId": power_station_id}
 
