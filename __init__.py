cae6c75b58d2d71aff92f9e56bc6e3dc86f0f3b9
Andrew Gierens <apgierens@gmail.com>
2023-11-03 09:24:05 +0000

    Changes

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .sems_auth import login_to_sems
+from .sems_charts import get_plant_power_chart
+from .sems_plant_details import get_plantDetails, get_powerflow
--- sems_plant_details.py
+++ sems_plant_details.py
@@ -17,3 +17,18 @@ async def get_plantDetails(
     response_data = await response.json()
 
     return response_data["data"]
+
+
+async def get_powerflow(
+    session: ClientSession, power_station_id: str, token: str
+) -> Any:
+    """Get the powerflow data."""
+
+    url = "https://au.semsportal.com/api/v2/PowerStation/GetPowerflow"
+    headers = {"Content-Type": "application/json", "Token": token}
+    body = {"PowerStationId": power_station_id}
+
+    response = await session.post(url, headers=headers, json=body)
+    response_data = await response.json()
+
+    return response_data["data"]
